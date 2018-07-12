c8b9b31da618e7ac566eed178e4e780cdeca5cf2
e.ruiz <e.ruiz@mbcgroup.mx>
2018-07-12 14:39:36 -0500

    Recibos 12.6

--- recibos/Modulos/mdoInicio.vb
+++ recibos/Modulos/mdoInicio.vb
@@ -146,12 +146,12 @@ Module mdoInicio
                 'Servidor.IP = "facturacion.elchingon.net"
 
                 ' Servidor.IP = "localhost"
-                Servidor.IP = "192.168.1.223"
+                Servidor.IP = "192.168.1.222"
                 Servidor.Nombre = "Equipo1\sqlexpress"
                 Servidor.Base = "unificacion"
                 Servidor.User = "sa"
                 'Servidor.PWD = "123456"
-                Servidor.PWD = "1123581321"
+                Servidor.PWD = "kiosko2016"
 
             End If
             strConfig.WriteLine(Servidor.IP)
