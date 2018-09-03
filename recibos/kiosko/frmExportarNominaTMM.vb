c5d26bdf798d25c3d93d12a904d61d4887b605f7
e.ruiz <e.ruiz@mbcgroup.mx>
2018-09-03 13:36:58 -0500

    RECIBOS

--- recibos/kiosko/frmExportarNominaTMM.vb
+++ recibos/kiosko/frmExportarNominaTMM.vb
@@ -80,8 +80,10 @@ Public Class frmExportarNominaTMM
                             SQL = "EXEC setpagoInsertar  0," & rwUsuarioK(0)("IdUsuario").ToString() & ",'" & Date.Parse(Trim(producto.SubItems(1).Text)).ToShortDateString() & "',1,"
                             SQL &= Double.Parse(Trim(producto.SubItems(4).Text).Replace(",", "").Replace("$", "").ToString()) & ","
                             SQL &= Double.Parse(Trim(producto.SubItems(6).Text).Replace(",", "").Replace("$", "").ToString()) & ",'"
-                            SQL &= Trim(producto.SubItems(5).Text) & "','','','" & Trim(producto.SubItems(7).Text) & "',''"
-
+                            SQL &= Trim(producto.SubItems(5).Text)
+                            SQL &= "','','','"
+                            SQL &= Trim(producto.SubItems(7).Text) & "',"
+                            SQL &= IIf(chkNominaB.Checked = True, txtcarpeta.Text, "")  'dsa es igual a nominaB
 
 
                             If nExecuteKiosko(SQL) = False Then
