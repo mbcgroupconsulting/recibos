2a9981f2ae960c64d7a53b4d2c4886c9dbf436df
Dereckz <dereckz1304@gmail.com>
2020-11-12 15:36:03 -0600

    Reportes finiqutio v1

--- recibos/My Project/Resources.Designer.vb
+++ recibos/My Project/Resources.Designer.vb
@@ -233,6 +233,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property excel__2_() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("excel (2)", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property GF1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("GF1", resourceCulture)
@@ -276,6 +286,16 @@ Namespace My.Resources
         Friend ReadOnly Property material_escolar() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("material-escolar", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property xls__2_() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("xls (2)", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
